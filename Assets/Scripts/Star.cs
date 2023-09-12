06e41c010602bd11f32935a0d54a323278f75ac9
VilleK88 <ville.karppinen88@gmail.com>
2023-09-12 09:11:48 +0300

    Working star -script made

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -40,6 +40,51 @@ public class Star : MonoBehaviour
         {
             StartCoroutine(SpawnStar10());
         }
+
+        if(scoreBool9Fetch)
+        {
+            StartCoroutine(SpawnStar9());
+        }
+
+        if(scoreBool8Fetch)
+        {
+            StartCoroutine(SpawnStar8());
+        }
+
+        if(scoreBool7Fetch)
+        {
+            StartCoroutine(SpawnStar7());
+        }
+
+        if(scoreBool6Fetch)
+        {
+            StartCoroutine(SpawnStar6());
+        }
+
+        if(scoreBool5Fetch)
+        {
+            StartCoroutine(SpawnStar5());
+        }
+
+        if(scoreBool4Fetch)
+        {
+            StartCoroutine(SpawnStar4());
+        }
+
+        if(scoreBool3Fetch)
+        {
+            StartCoroutine(SpawnStar3());
+        }
+
+        if(scoreBool2Fetch)
+        {
+            StartCoroutine(SpawnStar2());
+        }
+
+        if(scoreBool1Fetch)
+        {
+            StartCoroutine(SpawnStar1());
+        }
     }
 
     IEnumerator SpawnStar10()
@@ -49,6 +94,132 @@ public class Star : MonoBehaviour
         transform.position = childCastPointPositionFetch;
 
         GameObject starPrefab = Instantiate(star10, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar9()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star9, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar8()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star8, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar7()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star7, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar6()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star6, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar5()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star5, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar4()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star4, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar3()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star3, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar2()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star2, transform.position, Quaternion.identity);
+        Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(0, 2);
+
+        yield return new WaitForSeconds(1.5f);
+        Destroy(starPrefab);
+    }
+
+    IEnumerator SpawnStar1()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        transform.position = childCastPointPositionFetch;
+
+        GameObject starPrefab = Instantiate(star1, transform.position, Quaternion.identity);
         Rigidbody2D rb2d = starPrefab.GetComponent<Rigidbody2D>();
         rb2d.velocity = new Vector2(0, 2);
 
