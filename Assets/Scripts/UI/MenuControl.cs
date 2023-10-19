805c50499138fda0b523cae57243f22ed469ab40
VilleK88 <ville.karppinen88@gmail.com>
2023-10-19 11:13:43 +0300

    Work in progress

--- Assets/Scripts/UI/MenuControl.cs
+++ Assets/Scripts/UI/MenuControl.cs
@@ -1,8 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuControl : MonoBehaviour
 {
+    //public Button[] buttonsList;
+    public List<Button> buttonsList = new List<Button>();
+    int selectedButtonIndex = 0;
+    public Button listen;
+    public Button guide;
+    public Button again;
+    public Button exit;
+
+    private void Start()
+    {
+        //listen = FindObjectOfType<ListenButton>();
+        Button[] foundButtons = FindObjectsOfType<Button>();
+        for(int i = 0; i < foundButtons.Length; i++)
+        {
+            buttonsList.Add(foundButtons[i]);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            //buttons[selectedButtonIndex].onClick.Invoke();
+            buttonsList[selectedButtonIndex].onClick.Invoke();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //selectedButtonIndex = (selectedButtonIndex + 1) % buttons.Length;
+            //selectedButtonIndex++;
+            /*if(selectedButtonIndex >= buttons.Length)
+            {
+                selectedButtonIndex = 0;
+            }*/
+            selectedButtonIndex = (selectedButtonIndex + 1) % buttonsList.Count;
+        }
+    }
+
     public void LoadGame()
     {
         SceneManager.LoadScene("Dart");
